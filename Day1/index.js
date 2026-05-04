62e12c9cfca64f47f4cd653b5e8420419ebd1175
suryavikram2003 <suryavikramkrishnan@gmail.com>
2026-05-04 14:09:41 +0530

    string operations, mathematical operations , logical operations are done

---
+++ Day1/index.js
@@ -0,0 +1,32 @@
+// console.log(4 + 5);
+
+// let a =5;
+// let b = 6;
+// console.log(a + b);
+
+// //Mathematical Operations 
+// console.log("Addition: " + (a + b));
+// console.log("Subtraction: " + (a - b));
+// console.log("Multiplication: " + (a * b));
+// console.log("Division: " + (a / b));
+// console.log("Modulo: " + (a % b)); 
+
+
+let str = "Hello, World!";
+console.log("String: " + str);
+// String Length
+console.log("Length of the string: " + str.length);
+console.log("Uppercase: " + str.toUpperCase());
+console.log("Lowercase: " + str.toLowerCase());
+console.log("Substring (0, 5): " + str.substring(0, 5));
+// 0 - first character, 5 - last character (not included)
+
+//Logical Operations
+let c = 5;
+let d = 10;
+console.log("c > d: " + (c > d)); // false
+console.log("c < d: " + (c < d));
+console.log("c == 5: " + (c == 5)); // true
+console.log("c != 5: " + (c != 5));
+console.log("c >= d: " + (c >= d));
+console.log("c <= d: " + (c <= d));
