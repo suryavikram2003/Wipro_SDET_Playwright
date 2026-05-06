6a5e3d6364fcdf456812100b1e89f5bb3830f42a
suryavikram2003 <suryavikramkrishnan@gmail.com>
2026-05-06 12:44:50 +0530

    polymorphism

---
+++ Day5/polymorphism.js
@@ -0,0 +1,24 @@
+class Shape 
+{
+    draw()
+    {
+        console.log("Drawing a shape");
+    }
+}
+class Circle extends Shape
+{
+    draw()
+    {
+        console.log("Drawing a circle");
+    }
+}
+class Square extends Shape
+{
+    draw()
+    {
+        console.log("Drawing a square");
+    }   
+}
+
+const shapes = [new Circle(), new Square(), new Shape()];
+shapes.forEach(shape => shape.draw());
