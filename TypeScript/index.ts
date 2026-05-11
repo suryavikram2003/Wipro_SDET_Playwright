aa2dbcb5fd595ba0515254e5667eae22d8cf701e
suryavikram2003 <suryavikramkrishnan@gmail.com>
2026-05-11 12:42:01 +0530

    Basics of TS

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,2 +1,16 @@
-let name = "Surya";
-console.log(name);
+// let name = "Surya";
+// console.log(name);
+
+
+let name: string = "Surya";
+
+let result: number | string;
+result = 42; // Valid
+result = "Hello"; // Valid
+console.log(name, result);
+
+
+function add(a:number, b:number):string{
+    return "Sum is:" +(a+b);
+}
+console.log(add(5,10));
